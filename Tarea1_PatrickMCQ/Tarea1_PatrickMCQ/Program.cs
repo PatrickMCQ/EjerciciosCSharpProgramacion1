8a5a96c9d0964a7f9aaa0132c35ea7e4bae151e8
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 01:55:49 -0600

    Tarea 1

---
+++ Tarea1_PatrickMCQ/Tarea1_PatrickMCQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea_1_Impresion_de_numeros_del_0_al_100
+{
+    class numeros0a100
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
