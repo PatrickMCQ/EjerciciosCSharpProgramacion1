0d0fa34e826dd62a8dd7703be030a57511d38749
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 02:42:07 -0600

    Tarea 17

---
+++ Tarea17_NumeroParImpar/Tarea17_NumeroParImpar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tarea17_NumeroParImpar
+{
+    class Program17
+    {
+        static float numero;
+        static string entrada;
+        static void Main(string[] args)
+        {
+            numero = 0;  //Variable útil para conversión.
+            entrada = "";  //Variable para entrada de datos.
+
+            numeroPositivoNegativo();
+            Console.ReadKey();
+        }
+
+        static private void numeroPositivoNegativo()
+        {
+            Console.WriteLine("Ingrese un número para saber si es par o impar: ");  //Se solicita al usuario el ingreso de datos.
+            entrada = Console.ReadLine();  //Se le asigna al entrada el dato recibido.
+            numero = Convert.ToSingle(entrada);  //Se hace la conversión de string a float, y se le asigna el valor de entrada tipo float.
+
+            if (numero % 2 == 0)  //Si numero es mayor a 0 Entonces:
+            {
+                Console.WriteLine("El número ingresado es par: ");  //Se muestra al usuario que si número es par, si se cumple la condición.
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado es impar: ");  //Se muestra en pantalla que el numero es impar, si no se cumple la función.
+            }
+        }
+    }
+}
+
