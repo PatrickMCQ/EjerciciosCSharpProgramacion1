6c059a8f6787ee2c76c71159543017cb11fc4f82
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 02:19:41 -0600

    Tarea 8

---
+++ Tarea8_MultiplosDe3/Tarea8_MultiplosDe3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tarea8_MultiplosDe3
+{
+    class Tarea8
+    {
+        static int i, conteo;
+        static void Main(string[] args)
+        {
+            i = 3;  //Iterador del bucle while.
+            conteo = 0;  //Esta variable me contará cuantos multiplos de 3 hay.
+
+            multiplosDe3De1a100();
+            Console.ReadKey();
+        }
+
+        static private void multiplosDe3De1a100()
+        {
+            while (i <= 100)  //Mientres i se menor o igual a 100 se realizará lo siguiente.
+            {
+                Console.WriteLine(i);  //Se muestra en pantalla el valor acumulado de i.
+                i = i + 3;  //Se suma (+ 3) al valor acumulado de i.
+                conteo = conteo + 1;  //Cuenta cuantas veces se repite el ciclo.
+            }
+
+            Console.WriteLine("Existe un total de: " + conteo + " múltiplos de 3");  //Se muestra en pantalla cuantos múltiplos de 3 hay.
+        }
+    }
+}
