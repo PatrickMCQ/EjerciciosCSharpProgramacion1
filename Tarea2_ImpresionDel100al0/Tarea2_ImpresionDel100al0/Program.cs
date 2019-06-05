ed4e666554d4092680cbe7b4957ecca01aa13827
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 02:00:22 -0600

    Tarea 2

---
+++ Tarea2_ImpresionDel100al0/Tarea2_ImpresionDel100al0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarea2_ImpresionDel100al0
+{
+    class numeros100a0
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+
+            while (i >= 0)
+            {
+                Console.WriteLine(i);
+                i = i - 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
