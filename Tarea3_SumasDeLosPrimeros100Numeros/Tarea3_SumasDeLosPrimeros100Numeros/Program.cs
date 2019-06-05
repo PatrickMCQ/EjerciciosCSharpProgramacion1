48c59e2a7a7196d9b4ed960da295236de1a56536
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 02:03:44 -0600

    Tarea 3

---
+++ Tarea3_SumasDeLosPrimeros100Numeros/Tarea3_SumasDeLosPrimeros100Numeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tarea3_SumasDeLos_Primeros100Numeros
+{
+    class cienPrimerosnumeros
+    {
+        static int i, suma;
+        static void Main(string[] args)
+        {
+            i = 1; //Iterador para el bucle while
+            suma = 0;
+
+            sumasDeNumeros();
+            Console.ReadKey();
+        }
+
+        static private void sumasDeNumeros()
+        {
+            while (i <= 100)
+            {
+                suma = suma + i; //La variable suma me acumulará la suma de mi iterador.
+                i = i + 1; //Mi variable i que es mi iterador me acumulara la suma de + 1 hasta llegar según la condición de While
+                Console.WriteLine(suma); //Esto me mostrará en pantalla las sumas. 
+            }
+        }
+    }
+
+}
