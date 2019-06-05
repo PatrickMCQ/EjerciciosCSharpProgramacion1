6bf92e735db031fd01ff733a6a2663a7a710396e
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 03:19:44 -0600

    Tarea 39

---
+++ Tarea39_SerieFibonacci/Tarea39_SerieFibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Tarea39_SerieFibonacci
+{
+    class Program
+    {
+        static int pre, pos, val;
+        static float numero;
+        static string entrada;
+        static void Main(string[] args)
+        {
+            //Asgnaciónd de varibales.
+            val = 0;
+            pre = 0;
+            pos = 1;
+            numero = 0;
+            entrada = "";
+
+            condicionalParaGeerarSerieFibonacci();
+            Console.ReadKey();
+        }
+
+        static private void condicionalParaGeerarSerieFibonacci()
+        {
+            //Se solicita una cantidad para generar los términos de la serie fibonacci
+            Console.WriteLine("Ingrese la cantidad de términos que quiere generar: ");
+            entrada = Console.ReadLine();
+            numero = Convert.ToSingle(entrada);
+
+            //Se muestra en pantalla el valor de pre y pos.
+            Console.WriteLine(pre + "  " + pos);
+
+            //Bucle para generar la serie fibonacci según la cantidad ingresda.
+            for (int i = 3; i <= numero; i++)  //i = Iterardor.
+            {
+                val = pre + pos;  //A val se le asignará la suma entre pre y pos.
+                pre = pos;  //A pre se le asigna el valor acumulado de pos.
+                pos = val;  //A pos se le asigna el valor acumulado de val.
+
+                Console.WriteLine(val);  //Se muestra en pantalla el valor acumulado de val.
+            }
+        }
+
+    }
+}
