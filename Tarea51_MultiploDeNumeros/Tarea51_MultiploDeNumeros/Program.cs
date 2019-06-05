539fd7b53ba59c7efc7e7fec5b8936941a3b5f97
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 03:43:08 -0600

    Tarea 51

---
+++ Tarea51_MultiploDeNumeros/Tarea51_MultiploDeNumeros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tarea51_MultiploDeNumeros
+{
+    class Program
+    {
+        static int num1, num2;
+        static string entrada;
+        static void Main(string[] args)
+        {
+            //Aisgnación de variables.
+            num1 = 0;
+            num2 = 0;
+            entrada = "";
+
+            //Ingreso de datos.
+            Console.WriteLine("Ingrese el primer número: ");
+            entrada = Console.ReadLine();
+            num1 = Convert.ToInt32(entrada);
+
+            Console.WriteLine("Ingrese el segundo número: ");
+            entrada = Console.ReadLine();
+            num2 = Convert.ToInt32(entrada);
+
+            //Método para saber si el primer número es múltiplo del segundo número.
+            multiploDelPrimero();
+            Console.ReadKey();
+        }
+
+        static private void multiploDelPrimero()
+        {
+            //Condicional para saber si el primer número es múltiplo del segundo.
+            if (num1 % num2 == 0)
+            {
+                Console.WriteLine(num1 + " es múltiplo de " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " no es múltiplo de " + num2);
+            }
+        }
+    }
+}
