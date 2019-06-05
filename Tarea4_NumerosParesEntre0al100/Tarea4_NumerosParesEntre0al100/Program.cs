6c9e99126854dfc839f3c8fe2a7464c42bc42c34
PatrickMCQ <patrickmcq7@gmail.com>
2019-06-05 02:11:01 -0600

    Tarea 4

---
+++ Tarea4_NumerosParesEntre0al100/Tarea4_NumerosParesEntre0al100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea4_NumerosParesEntre0al100
+{
+    class numerosPares
+    {
+        static int i;
+        static void Main(string[] args)
+        {
+            i = 0;
+
+            numerosPares0al100();
+            Console.ReadKey();
+        }
+
+        static private void numerosPares0al100()
+        {
+            while (i <= 100)
+            {
+                Console.WriteLine(i); //Me mostrará en pantalla lo acumulado de i
+                i = i + 2; //La variable i me acumulará la suma +2 hasta que se detenga según la condicón de While
+            }
+        }
+    }
+}
+
